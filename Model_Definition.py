21652ced6d40dbc2ed5c63c9e1a3828758298193
Ahk <48156018+ahk19@users.noreply.github.com>
2024-01-24 15:03:17 +0300

    Create Model_Definition.py

---
+++ Model_Definition.py
@@ -0,0 +1,60 @@
+def conv_block(input_tensor, num_filters):
+    x = Conv2D(num_filters, (3, 3), padding='same')(input_tensor)
+    x = BatchNormalization()(x)
+    x = Activation('relu')(x)
+    x = Conv2D(num_filters, (3, 3), padding='same')(x)
+    x = BatchNormalization()(x)
+    x = Activation('relu')(x)
+    return x
+
+def unet(input_size=(256, 256, 1), num_filters=64, dropout=0.5):
+    inputs = Input(input_size)
+
+    # Contracting Path
+    c1 = conv_block(inputs, num_filters)
+    p1 = MaxPooling2D((2, 2))(c1)
+    p1 = Dropout(dropout)(p1)
+
+    c2 = conv_block(p1, num_filters * 2)
+    p2 = MaxPooling2D((2, 2))(c2)
+    p2 = Dropout(dropout)(p2)
+
+    c3 = conv_block(p2, num_filters * 4)
+    p3 = MaxPooling2D((2, 2))(c3)
+    p3 = Dropout(dropout)(p3)
+
+    c4 = conv_block(p3, num_filters * 8)
+    p4 = MaxPooling2D(pool_size=(2, 2))(c4)
+    p4 = Dropout(dropout)(p4)
+
+    # Bottleneck
+    c5 = conv_block(p4, num_filters * 16)
+
+    # Expansive Path
+    u6 = UpSampling2D((2, 2))(c5)
+    u6 = concatenate([u6, c4])
+    u6 = Dropout(dropout)(u6)
+    c6 = conv_block(u6, num_filters * 8)
+
+    u7 = UpSampling2D((2, 2))(c6)
+    u7 = concatenate([u7, c3])
+    u7 = Dropout(dropout)(u7)
+    c7 = conv_block(u7, num_filters * 4)
+
+    u8 = UpSampling2D((2, 2))(c7)
+    u8 = concatenate([u8, c2])
+    u8 = Dropout(dropout)(u8)
+    c8 = conv_block(u8, num_filters * 2)
+
+    u9 = UpSampling2D((2, 2))(c8)
+    u9 = concatenate([u9, c1])
+    u9 = Dropout(dropout)(u9)
+    c9 = conv_block(u9, num_filters)
+
+    outputs = Conv2D(1, (1, 1), activation='sigmoid')(c9)
+
+    model = Model(inputs=[inputs], outputs=[outputs])
+    model.compile(optimizer=Adam(), loss='binary_crossentropy', metrics=['accuracy'])
+    return model
+
+model = unet()
